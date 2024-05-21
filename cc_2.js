0275e5d6741a1cbd49c60f18b175946145dec6e2
anjalikrishna-11 <123520060+anjalikrishna-11@users.noreply.github.com>
2024-05-21 14:47:12 -0400

    Create Car class with constructor for make and speed.

---
+++ cc_2.js
@@ -0,0 +1,7 @@
+// Anjali Krishna (U35346496)
+// Coding Challenge-3: Car Class Implementation 
+class Car {
+    constructor(make, speed) {
+        this.make = make;
+        this.speed = speed;
+    }
